fdaa3a603d470d1898f080f66bbad7e22316cb71
flooijdt <flooijdt@posteo.de>
2023-03-07 17:45:53 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use scraper::{Html, Selector};
 #[tokio::main]
 async fn main() {
     let url: &str = "https://roadmap.sh/blockchain";
-    let req = reqwest::get(url).await.unwrap().text();
+    let req = reqwest::get(url).await.unwrap().text().await.unwrap();
 
-    let body = Html::parse_document(&req.await.unwrap());
+    let body = Html::parse_document(&req);
     let matter = Selector::parse("li").unwrap();
 
     for m in body.select(&matter) {
