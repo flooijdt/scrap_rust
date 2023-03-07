301d7e0b7a95cdfc7b8e142d2a04c2f9a5e91fc7
flooijdt <flooijdt@posteo.de>
2023-03-07 16:52:49 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ use tokio;
 async fn main() {
     let url: &str = "https://helix-editor.com/";
     let mut req = reqwest::get(url).await;
+    println!("{:?}", &req);
 
-    let body = Html::parse_document(&req.unwrap().text());
-    let matter = Selector::parse(".matter").unwrap();
+    // let body = Html::parse_document(&req.unwrap().try_into().unwrap());
+    // let matter = Selector::parse(".matter").unwrap();
 
-    for m in body.select(&matter) {
-        let matters = m.text().collect::<Vec<_>>();
-        println!("{:#?}", &matters[0]);
-    }
+    // for m in body.select(&matter) {
+    //     let matters = m.text().collect::<Vec<_>>();
+    //     println!("{:#?}", &matters[0]);
+    // }
 }
