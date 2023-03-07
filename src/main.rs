a2809955e031cf35109ea253074bf68f04f3b8ec
flooijdt <flooijdt@posteo.de>
2023-03-07 18:12:54 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ async fn main() {
     let req = reqwest::get(url).await.unwrap().text().await.unwrap();
 
     let body = Html::parse_document(&req);
-    let matter = Selector::parse("li").unwrap();
+    let selection = Selector::parse("li").unwrap();
 
-    for m in body.select(&matter) {
-        let matters = m.text().collect::<Vec<_>>();
-        println!("{:#?}", &matters[0]);
+    for m in body.select(&selection) {
+        let selections = m.text().collect::<Vec<_>>();
+        println!("{:#?}", &selections[0]);
     }
 }
