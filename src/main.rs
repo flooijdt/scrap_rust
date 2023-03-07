c87a396e563c15db080fb9fe9426829d5904f8e4
flooijdt <flooijdt@posteo.de>
2023-03-07 16:29:46 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use reqwest;
+use scraper::{Html, Selector};
+
 fn main() {
-    println!("Hello, world!");
+    let url: &str = "https://helix-editor.com/";
+    let mut req = reqwest::get(url).unwrap();
+
+    let body = Html::parse_document(&req.text().unwrap());
+    let matter = Selector::parse(".matter").unwrap();
+
+    for m in body.select(&matter) {
+        let matters = m.text().collect::<Vec<_>>();
+        println!("{:#?}", &matters[0]);
+    }
 }
